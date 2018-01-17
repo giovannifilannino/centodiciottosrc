d3f6f9aea7eaaad6e7b4b648c110a7b4473163eb
giovanni <giovanni.filannino@advancia.it>
2018-01-17 12:41:05 +0100

    modifiche al software

--- src/main/java/com/gianni/webservice/TestWebService.java
+++ src/main/java/com/gianni/webservice/TestWebService.java
@@ -1,32 +1,18 @@
 package com.gianni.webservice;
 
-import java.security.Key;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-import java.time.LocalDateTime;
-import java.time.ZoneId;
-import java.util.Date;
 import java.util.List;
 import java.util.logging.Logger;
 
-import javax.crypto.KeyGenerator;
-import javax.inject.Inject;
 import javax.ws.rs.GET;
-import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
-import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
-
-import static javax.ws.rs.core.HttpHeaders.AUTHORIZATION;
-import static javax.ws.rs.core.Response.Status.UNAUTHORIZED;
 
 //import org.codehaus.commons.compiler.CompilerFactoryFactory;
 //import org.codehaus.commons.compiler.IClassBodyEvaluator;
@@ -35,54 +21,12 @@ import org.hibernate.query.Query;
 
 import com.gianni.dao.DBHibernateUtils;
 import com.gianni.model.Utente;
-import com.gianni.rest.JWTTokenNeeded;
-
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
 
 @Path("testWS")
 public class TestWebService {
 
 	Logger log = Logger.getLogger(TestWebService.class.getSimpleName());
 
-	@Inject
-	private KeyGenerator keyGenerator;
-
-	@Context
-	private UriInfo uriInfo;
-
-	@GET
-	public Response echo(@QueryParam("message") String message) {
-		return Response.ok().entity(message == null ? "no message" : message).build();
-	}
-
-	@GET
-	@Path("jwt")
-	@JWTTokenNeeded
-	public Response echoWithJWTToken(@QueryParam("message") String message) {
-		return Response.ok().entity(message == null ? "no message" : message).build();
-	}
-
-	@POST
-	@Path("/login/{:user}")
-	public Response authenticateUser(@PathParam("user") String user) {
-		try {
-			// Issue a token for the user
-			String token = issueToken(user);
-			// Return the token on the response
-			return Response.ok().header(AUTHORIZATION, "Bearer " + token).build();
-		} catch (Exception e) {
-			return Response.status(UNAUTHORIZED).build();
-		}
-	}
-
-	private String issueToken(String login) {
-		Key key = keyGenerator.generateKey();
-		String jwtToken = Jwts.builder().setSubject(login).setIssuer(uriInfo.getAbsolutePath().toString())
-				.setIssuedAt(new Date()).setExpiration(toDate(LocalDateTime.now().plusMinutes(15L)))
-				.signWith(SignatureAlgorithm.HS512, key).compact();
-		return jwtToken;
-	}
 
 	// @POST
 	// @Consumes("application/json")
@@ -163,7 +107,4 @@ public class TestWebService {
 		return test.toString();
 	}
 
-	private Date toDate(LocalDateTime localDateTime) {
-		return Date.from(localDateTime.atZone(ZoneId.systemDefault()).toInstant());
-	}
 }
